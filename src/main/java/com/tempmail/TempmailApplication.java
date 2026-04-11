ce1ade21785ad5044e8479171f6b36e8ee640b01
Vidhan <vidhanlonare@gmail.com>
2026-04-11 14:26:30 +0530

    initialized the project

---
+++ src/main/java/com/tempmail/TempmailApplication.java
@@ -0,0 +1,13 @@
+package com.tempmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempmailApplication.class, args);
+	}
+
+}
